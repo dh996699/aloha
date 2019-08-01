4c521f74cbd8992fb1b4a0f61bc77713888d5950
David Huynh <dh99.66.99@gmail.com>
2019-08-01 16:09:47 -0700

    testing with compress image and skip the line

--- javascript/add-to-cart.js
+++ javascript/add-to-cart.js
@@ -9,42 +9,47 @@ const blsidr = document.getElementById("blue-silk-dress");
 const coyets = document.getElementById("cotton-yellow-tshirt");
 
 count = 0;
-  stkndr.onclick = function() {
+stkndr.onclick = function () {
   count += 1;
   addcount.innerHTML = count;
 };
 
-coblsh.onclick = function() {
-    count += 1;
-    addcount.innerHTML = count;
-  };
+coblsh.onclick = function () {
+  count += 1;
+  addcount.innerHTML = count;
+};
 
-  chstdr.onclick = function() {
-    count += 1;
-    addcount.innerHTML = count;
-  };
+chstdr.onclick = function () {
+  count += 1;
+  addcount.innerHTML = count;
+};
 
-  blhiba.onclick = function() {
-    count += 1;
-    addcount.innerHTML = count;
-  };
+blhiba.onclick = function () {
+  count += 1;
+  addcount.innerHTML = count;
+};
 
-  deovsh.onclick = function() {
-    count += 1;
-    addcount.innerHTML = count;
-  };
+deovsh.onclick = function () {
+  count += 1;
+  addcount.innerHTML = count;
+};
 
-  pidodr.onclick = function() {
-    count += 1;
-    addcount.innerHTML = count;
-  };
+pidodr.onclick = function () {
+  count += 1;
+  addcount.innerHTML = count;
+};
 
-  blsidr.onclick = function() {
-    count += 1;
-    addcount.innerHTML = count;
-  };
+blsidr.onclick = function () {
+  count += 1;
+  addcount.innerHTML = count;
+};
 
-  coyets.onclick = function() {
-    count += 1;
-    addcount.innerHTML = count;
-  };
+coyets.onclick = function () {
+  count += 1;
+  addcount.innerHTML = count;
+};
+
+#main[tabindex = "-1"]: focus {
+  outline: 0;
+  visibility: hidden;
+};
